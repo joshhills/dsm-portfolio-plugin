836d540fe5167e0693983947935affe1fca4de45
Josh Hills <josh.hills@ymail.com>
2018-08-26 17:12:14 +0100

    Updated version

--- lib/dsm-portfolio-plugin.rb
+++ lib/dsm-portfolio-plugin.rb
@@ -2,58 +2,69 @@
 module Jekyll
     # Define custom commands.
     module Commands
-        class Post < Command
+        class NewProject < Command
             def self.init_with_program(prog)
-              prog.command(:post) do |c|
-                c.syntax 'post NAME'
-                c.description 'Creates a new post with the given NAME'
-      
-                c.option 'type', '-t TYPE', '--type TYPE', 'Specify the content type'
-                c.option 'layout', '-t LAYOUT', '--layout LAYOUT', 'Specify the post layout'
-                c.option 'date', '-d DATE', '--date DATE', 'Specify the post date'
-                c.option 'force', '-f', '--force', 'Overwrite a post if it already exists'
-      
-                c.action do |args, options|
-                  Jekyll::Commands::Post.process(args, options)
+                prog.command(:project) do |c|
+                    c.syntax 'project NAME'
+                    c.description 'Creates a new project with the given NAME'
+        
+                    c.option 'date', '-d DATE', '--date DATE', 'Specify the post date'
+                    c.option 'force', '-f', '--force', 'Overwrite a post if it already exists'
+        
+                    c.action do |args, options|
+                        Jekyll::Commands::Project.process(args, options)
+                    end
                 end
-              end
             end
-      
+
             def self.process(args, options = {})
-              raise ArgumentError.new('You must specify a name.') if args.empty?
-              
-              type = options["type"].nil? ? "markdown" : options["type"]
-              layout = options["layout"].nil? ? "post" : options["layout"]
-      
-              date = options["date"].nil? ? Time.now : DateTime.parse(options["date"])
-      
-              title = args.shift
-              name = title.gsub(' ', '-').downcase
-      
-              post_path = file_name(name, type, date)
-      
-              raise ArgumentError.new("A post already exists at ./#{post_path}") if File.exist?(post_path) and !options["force"]
-      
-              File.open(post_path, "w") do |f|
-                f.puts(front_matter(layout, title))
-              end
-      
-              puts "New post created at ./#{post_path}.\n"
+                raise ArgumentError.new('You must specify a project name.') if args.empty?
+
+                # layout = options["layout"].nil? ? "post" : options["layout"]
+                
+                # Create options for each post type.
+                ext = "md"
+                date = options["date"].nil? ? Time.now : DateTime.parse(options["date"])
+
+                title = args.shift
+                name = title.gsub(' ', '-').downcase
+
+                # TODO: Replace this with smart filling from data file and template file.
+                post_types = []
+
+                post_types.push('vignette')
+                post_types.push('summary')
+
+                # raise ArgumentError.new("A post already exists at ./#{post_path}") if File.exist?(post_path) and !options["force"]
+
+                # For every post-type in subfolder...
+                post_types.each do |file_type|
+                    # Format file path.
+                    post_path = file_name(name, post_type, ext, date)
+                    
+                    # Create file.
+                    File.open(post_path, "w") do |f|
+                        # Fill it with appropriate front-matter.
+                        f.puts(front_matter(post_type, title))
+                    end
+                end
+
+                puts "New posts created at ./_posts/#{date.strftime('%Y-%m-%d')}-#{name}.\n"
             end 
-            # Internal: Gets the filename of the draft to be created
-            #
+            
             # Returns the filename of the draft, as a String
-            def self.file_name(name, ext, date)
-              "_posts/#{date.strftime('%Y-%m-%d')}-#{name}.#{ext}"
+            def self.file_name(name, post_type, ext, date)
+                "_posts/#{date.strftime('%Y-%m-%d')}-#{name}/#{post_type}.#{ext}"
             end
-      
+
+            # TODO: Replace this with smart filling from data file and template file.
             def self.front_matter(layout, title)
-              "---
-      layout: #{layout}
-      title: #{title}
-      ---"
+                "---
+                layout: #{layout}
+                title: #{title}
+                ---"
             end
-          end
+        end
     end
 
     class ProjectGenerator < Generator
