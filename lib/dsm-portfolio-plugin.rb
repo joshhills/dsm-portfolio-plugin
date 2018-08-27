dc411d0d583455838a9f0a89706f6ffa056d1972
Josh Hills <josh.hills@ymail.com>
2018-08-27 21:47:32 +0100

    Updated version

--- lib/dsm-portfolio-plugin.rb
+++ lib/dsm-portfolio-plugin.rb
@@ -39,7 +39,7 @@ module Jekyll
                 site = Jekyll::Site.new(config_options)
                 
                 puts "test: "
-                puts site.to_json
+                puts site.data.to_json
 
                 if post_types.size > 0
                     # Make subdirectory.
